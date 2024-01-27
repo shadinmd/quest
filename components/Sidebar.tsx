9f8e5b8ae44e5ab6a15e7bf1f6f512373a887aca
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-01-27 21:04:05 +0530

    change: sidebar shows groups

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,28 +1,62 @@
 "use client"
 import { useAuth } from "@/context/AuthContext"
 import cn from "@/lib/cn"
-import { Icon } from "@iconify/react/dist/iconify.js"
+import { Icon } from "@iconify/react"
 import Link from "next/link"
+import YesNoDialog from "./YesNoDialog"
+import { useState } from "react"
+import GroupInterface from "@/interface/group.interface"
+import NewGroup from "./group/NewGroup"
+import { useGroup } from "@/context/GroupContext"
 
 type className = string
 const linkStyle: className = "flex gap-3 items-center text-xl text-black bg-white px-5 py-2 font-bold rounded-xl border-2"
 
 const Sidebar = () => {
 	const { logout, loggedIn } = useAuth()
+	const { groups, setGroups } = useGroup()
+	const [newGroup, setNewGroup] = useState(false)
+
+	const addGroup = (group: GroupInterface) => {
+		const temp = [...groups]
+		temp.push(group)
+		setGroups(temp)
+	}
 
 	return (
-		<div className="flex flex-col justify-between h-full py-5">
-			<div className="flex flex-col gap-3">
-				<Link className={linkStyle} href={"/"}>Home</Link>
-				<Link className={linkStyle} href={"/daily"}>Daily</Link>
-				<Link className={linkStyle} href={"/bucketlist"}>Bucket list</Link>
+		<div className="flex flex-col gap-4 justify-between h-full py-5">
+			<Link className={linkStyle} href={"/"}>Tasks</Link>
+			<div className="flex h-full w-full items-center justify-center overflow-auto">
+				<div className="flex flex-col gap-2 items-center justify-start h-full">
+					{
+						groups.length > 0 ? groups.map((e, i) => (
+							<Link
+								key={i}
+								href={`/g/${e?._id}`}
+								className="flex flex-col gap-1 w-full font-bold items-center border border-white rounded-lg hover:border-black cursor-pointer"
+							>
+								{e?.name}
+							</Link>
+						))
+							:
+							<p>Empty</p>
+					}
+				</div>
 			</div>
 			<div className="flex flex-col gap-3">
+				<NewGroup open={newGroup} onOpenChange={setNewGroup} addGroup={addGroup}>
+					<div className="flex items-center justify-center w-full rounded-xl border-2">
+						<Icon icon={"typcn:plus"} className="text-2xl text-green-500 py-1" />
+					</div>
+				</NewGroup>
+				<Link className={linkStyle} href={"/bucketlist"}>Bucket list</Link>
 				{
 					loggedIn &&
-					<button className={cn(linkStyle, "text-red-500")} onClick={() => logout()}>
-						Logout
-					</button>
+					<YesNoDialog onYes={() => logout()}>
+						<div className={cn(linkStyle, "text-red-500 w-full h-full")}>
+							Logout
+						</div>
+					</YesNoDialog>
 				}
 				<Link className={linkStyle} href={"/settings"}>
 					<Icon icon={"mdi:gear"} />
