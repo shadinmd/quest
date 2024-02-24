0cf8b057b1a494227a6efc3c2be15aed3f416172
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2024-02-24 10:30:40 +0530

    update: made quest logo a link to / path

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { useAuth } from "@/context/AuthContext"
+import Link from "next/link"
 
 const Navbar = () => {
 	const { user } = useAuth()
 
 	return (
 		<header className="flex justify-between px-10 py-5 w-full">
-			<p className="text-3xl font-bold">Quest</p>
+			<Link href="/" className="text-3xl font-bold">Quest</Link>
 			<div className="flex gap-4 items-center">
 				{user.username &&
 					<p className="text-lg font-bold">{user?.username}</p>
